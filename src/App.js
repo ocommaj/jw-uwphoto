0e81586f004a8492df114f183f79a43796429b16
ocommaj <ocommaj@gmail.com>
2021-05-26 11:23:19 -0500

    refactor css for transition between gradient BGs

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         { activeSection === 'contact' && <ContactSection /> }
         { activeSection === 'gallery' && <GallerySection /> }
       </MainContentContainer>
+
     </div>
   );
 }
