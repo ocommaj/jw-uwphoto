85660514f088808acc01953145427a0b753dea82
ocommaj <ocommaj@gmail.com>
2021-05-27 13:13:12 -0500

    style input with placeholder text moving

--- src/ContentSections/ContactSection/ContactSection.js
+++ src/ContentSections/ContactSection/ContactSection.js
@@ -6,14 +6,34 @@ export function ContactSection() {
   return (
     <Jumbotron props={ { className: "contact-section" } }>
       <form className="contact-form">
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" placeholder="Name"/>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" placeholder="name@domain.tld"/>
-        <label htmlFor="location">Location</label>
-        <input id="location" type="text" placeholder="Location"/>
-        <label htmlFor="message">Message</label>
-        <textarea id="message" resize="none" rows={5} placeholder="Message"/>
+        <label className="styled-label" htmlFor="name" aria-label="Name">
+          <input
+            className="styled-input"
+            id="name"
+            type="text"
+            placeholder="&nbsp;"/>
+          <span class="placeholder">Name</span>
+        </label>
+        <label className="styled-label" htmlFor="email" aria-label="Email">
+          <input
+            className="styled-input"
+            id="email"
+            type="text"
+            placeholder="&nbsp;"/>
+          <span class="placeholder">Email</span>
+        </label>
+        <label className="styled-label" htmlFor="location" aria-label="Location">
+          <input
+            className="styled-input"
+            id="location"
+            type="text"
+            placeholder="&nbsp;"/>
+          <span class="placeholder">Location</span>
+        </label>
+        <label className="styled-label" htmlFor="message" aria-label="Message">
+          <textarea className="styled-input" id="message" placeholder="&nbsp;" rows={5}/>
+          <span class="placeholder">Message</span>
+        </label>
         <button>Submit</button>
       </form>
     </Jumbotron>
