684ba6642e942a81e9334d9f5be30253e6ce8607
ocommaj <ocommaj@gmail.com>
2021-05-29 13:35:39 -0500

    add gallery card demo example

--- src/Components/ImageCard/ImageCard.js
+++ src/Components/ImageCard/ImageCard.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './image-card.scss'
 
+import TEST_IMG from '../../Assets/images/RedSeaHorseSample.jpg';
+
 export function ImageCard() {
+
+
   return (
     <div className='image-card-circle'>
-      <ul>
+      <div className='image-card-img-front'>
+        <img src={TEST_IMG} alt='red sea pony'/>
+      </div>
+      <ul className='image-card-back-details'>
         <li>Red Sea Horse</li>
-        <li>West Palm Beach, FL</li>
-        <li>f8.0, 1/250</li>
+        <li className='icon-right-list-item'>
+          <span>West Palm Beach, FL</span>
+          <i className="fas fa-map-marker-alt" style={ {color: '#ec3750' } }/>
+        </li>
+        <li>50mm, f8.0, 1/500, ISO 1000</li>
       </ul>
     </div>
   )
