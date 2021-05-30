0050053227fb9b6ad1b70b2be319b79e7abe9b17
ocommaj <ocommaj@gmail.com>
2021-05-29 21:51:04 -0500

    background expander from image card

--- src/Components/ImageCard/ImageCard.js
+++ src/Components/ImageCard/ImageCard.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './image-card.scss'
 
 import TEST_IMG from '../../Assets/images/RedSeaHorseSample.jpg';
 
 export function ImageCard() {
-
+  const [ topClassName, setTopClassName ] = useState('image-card-circle')
 
   return (
-    <div className='image-card-circle'>
-      <div className='image-card-img-front'>
-        <img src={TEST_IMG} alt='red sea pony'/>
+    <div className='image-card-wrapper'>
+      <div className={ topClassName } onClick={ clickExpander }>
+        <div className='image-card-img-front'>
+          <img src={TEST_IMG} alt='red sea pony'/>
+        </div>
+        <ul className='image-card-back-details'>
+          <li>Red Sea Horse</li>
+          <li className='icon-right-list-item'>
+            <span>West Palm Beach, FL</span>
+            <i className="fas fa-map-marker-alt" style={ {color: '#ec3750'} }/>
+          </li>
+          <li>50mm, f8.0, 1/500, ISO 1000</li>
+        </ul>
       </div>
-      <ul className='image-card-back-details'>
-        <li>Red Sea Horse</li>
-        <li className='icon-right-list-item'>
-          <span>West Palm Beach, FL</span>
-          <i className="fas fa-map-marker-alt" style={ {color: '#ec3750' } }/>
-        </li>
-        <li>50mm, f8.0, 1/500, ISO 1000</li>
-      </ul>
+      <div className='active-container'></div>
     </div>
   )
+
+  function clickExpander() {
+    if (topClassName === 'image-card-circle') {
+      setTopClassName('image-card-circle active-select')
+    } else {
+      setTopClassName('image-card-circle')
+    }
+  }
 }
+
+//<div className='image-card-circle'>
--- src/ContentSections/GallerySection/GallerySection.js
+++ src/ContentSections/GallerySection/GallerySection.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { ImageCard } from '../../Components'
+import './gallery-section.scss'
 
 export function GallerySection() {
+
   return (
-    <>
+    <div className='particle-gallery'>
       <ImageCard />
-    </>
+    </div>
   )
 }
-
-//<Jumbotron props={ { className: 'gallery-section' } }></Jumbotron>
